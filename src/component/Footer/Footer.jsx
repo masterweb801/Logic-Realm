68a3dcc103f5f00950e5e0a909482c45322a400a
MD Mobashshirul Karim <masterweb801@gmail.com>
2025-10-14 03:17:28 +0600

    Some improvements in the navbar and footer

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Footer.css'
 import Lexaun from '../../assets/lexaun.png'
 
 const Footer = () => {
+    const [Scrolled, setScrolled] = useState(false);
+    const handleScroll = () => {
+        let height = window.screen.height;
+        if (window.scrollY > document.body.scrollHeight - 1.25 * height) {
+            setScrolled(false);
+        } else {
+            setScrolled(window.scrollY > 0.75 * height);
+        }
+    }
+
     const scrollToTop = () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+    }, []);
 
     return (
         <div className="footer">
@@ -27,7 +41,7 @@ const Footer = () => {
 
                 </div>
 
-                <button className="back-to-top" onClick={scrollToTop}>
+                <button className={Scrolled ? "back-to-top actop" : "back-to-top"} onClick={scrollToTop}>
                     <i className="fas fa-arrow-up"></i> &nbsp; Back to Top
                 </button>
             </div>
