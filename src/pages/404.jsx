86166c7cc5ebee61679f2155c821b2ddd6e8b52e
MD Mobashshirul Karim <masterweb801@gmail.com>
2025-10-28 00:09:43 +0600

    Fixed bug in error page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import './css/404.css'
 
 const ErrorPage = () => {
+    const eyeRef = useRef(null);
+    const eyeballRef = useRef(null);
+
     function eyeball() {
-        let eye = document.querySelector('.eye');
-        let eyeball = document.querySelector('.eyeball');
+        let eye = eyeRef.current;
+        let eyeball = eyeballRef.current;
+
+        if (!eye || !eyeball) return;
 
         let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
         let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
@@ -14,24 +19,40 @@ const ErrorPage = () => {
 
         let rot = (radian * (180 / Math.PI) * -1) + 270;
         eye.style.transform = "rotate(" + rot + "deg)";
-
-        eyeball.addEventListener("mouseover", function () {
-            eyeball.style.left = "50%";
-        })
-        eyeball.addEventListener("mouseout", function () {
-            eyeball.style.left = "36%";
-        })
     }
+
+    const handleMouseOver = () => {
+        const eyeball = eyeballRef.current;
+        if (eyeball) eyeball.style.left = '50%';
+    };
+
+    const handleMouseOut = () => {
+        const eyeball = eyeballRef.current;
+        if (eyeball) eyeball.style.left = '36%';
+    };
 
     useEffect(() => {
         document.addEventListener("mousemove", eyeball);
+        const eyeballEl = eyeballRef.current;
+        if (eyeballEl) {
+            eyeballEl.addEventListener('mouseover', handleMouseOver);
+            eyeballEl.addEventListener('mouseout', handleMouseOut);
+        }
+
+        return () => {
+            document.removeEventListener("mousemove", eyeball);
+            if (eyeballEl) {
+                eyeballEl.removeEventListener('mouseover', handleMouseOver);
+                eyeballEl.removeEventListener('mouseout', handleMouseOut);
+            }
+        }
     }, []);
     return (
         <div className='container_404'>
             <div className="text_404">
                 <h1>4</h1>
-                <div className="eye">
-                    <div className="eyeball"></div>
+                <div className="eye" ref={eyeRef}>
+                    <div className="eyeball" ref={eyeballRef}></div>
                 </div>
                 <h1>4</h1>
             </div>
